4da6e2e886e263ba87db391ba4650596f72b827f
elf1218 <kevinmu01@gmail.com>
2014-02-02 15:58:40 -0500

    hw2 start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hw2::Application.config.secret_key_base = '5b09ba466e694f09fd996e289b6887969685193da0c8a89492691fc3d7398bc4290def613bd49cfb002a8aa1e923f19937817c3e756fccb6b8eb8b765ba3cb21'
