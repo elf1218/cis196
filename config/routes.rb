590775734a9a1ecac0905d3c5cc8c7649db3571f
elf1218 <kevinmu01@gmail.com>
2014-02-02 20:06:30 -0500

    hw2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Hw2::Application.routes.draw do
+  root 'home#index'
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
